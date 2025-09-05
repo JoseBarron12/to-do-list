24a005098edaf1f7c11ffbe54e698865c42d8840
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-09-05 00:03:09 -0500

    Add close functionailty menu button
    Menu button when clicked now closes navbar section

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import "./styles.css";
 
 console.log("test3334");
+
+const menuButton = document.querySelector(".menu-tab>svg");
+
+menuButton.addEventListener("click", () => {
+    const main = document.querySelector("main");
+    const mainContent = document.querySelector(".main-content");
+
+    if(main.children.length == 2) // sidebar opened
+    {
+        main.children[0].remove();
+        mainContent.style.width = "100%";
+
+    }
+    else // sidebar close
+    {
+
+    }
+
+    
+});
