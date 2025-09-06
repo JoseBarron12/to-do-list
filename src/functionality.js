d7a4a9c8632bd67bd079c3bd8f3da7e4d047c8bb
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-09-05 22:15:59 -0500

    Move dropdownmenu functionailty to own module
    Dropdownmenu functionality implementation no longer
    found in index.js but as an IIFE under functionality
    as it is a button.

--- src/functionality.js
+++ src/functionality.js
@@ -50,6 +50,38 @@ export const functionality = (function () {
 
         });
     };
+
+    const addDropdownMenuBtn = () => 
+    {
+        const dropdownMenus = document.querySelectorAll(".dropdown-menu");
+
+        dropdownMenus.forEach((dropdownMenu) => {
+            dropdownMenu.addEventListener("click", () => {
+                
+                const upIcon = dropdownMenu.querySelector(".up");
+                const downIcon = dropdownMenu.querySelector(".down");
+
+                const isOpen = dropdownMenu.classList.toggle("open");
+
+                const elementToHide = dropdownMenu.parentNode.nextElementSibling;
+
+                if(isOpen)
+                {
+                    upIcon.style.opacity = "100%";
+                    downIcon.style.opacity = "0%"
+                    elementToHide.style.display = "flex";
+                }
+                else
+                {
+                    upIcon.style.opacity = "0%";
+                    downIcon.style.opacity = "100%";
+                    elementToHide.style.display = "none"
+
+                }
+            });
+        });
+    };
+
     
-    return {addMenuBtn, addThemeBtn};
+    return {addMenuBtn, addThemeBtn, addDropdownMenuBtn};
 })();
--- src/index.js
+++ src/index.js
@@ -3,32 +3,5 @@ import { functionality } from "./functionality";
 
 functionality.addMenuBtn();
 functionality.addThemeBtn();
-
-const dropdownMenus = document.querySelectorAll(".dropdown-menu");
-
-dropdownMenus.forEach((dropdownMenu) => {
-    dropdownMenu.addEventListener("click", () => {
-        
-        const upIcon = dropdownMenu.querySelector(".up");
-        const downIcon = dropdownMenu.querySelector(".down");
-
-        const isOpen = dropdownMenu.classList.toggle("open");
-
-        const elementToHide = dropdownMenu.parentNode.nextElementSibling;
-
-        if(isOpen)
-        {
-            upIcon.style.opacity = "100%";
-            downIcon.style.opacity = "0%"
-            elementToHide.style.display = "flex";
-        }
-        else
-        {
-            upIcon.style.opacity = "0%";
-            downIcon.style.opacity = "100%";
-            elementToHide.style.display = "none"
-
-        }
-    });
-});
+functionality.addDropdownMenuBtn();
 
