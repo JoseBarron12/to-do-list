6097b59b3f2d2ebfc7f396a4d5ec09bacf097e25
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-09-05 21:49:03 -0500

    Add menu dropdown icon + switch for task-sec-head
    Task section header has drop down menu icon up and
    down depending on user click.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,26 @@ import { functionality } from "./functionality";
 
 functionality.addMenuBtn();
 functionality.addThemeBtn();
+
+
+const dropdownMenu = document.querySelector(".dropdown-menu");
+
+dropdownMenu.addEventListener("click", () => {
+    const currentDirection = dropdownMenu.getAttribute("id");
+    const newDirection = (currentDirection) == "up" ? "down" : "up";
+    dropdownMenu.setAttribute("id", `${newDirection}`);
+
+    const upIcon = document.querySelector(".up");
+    const downIcon = document.querySelector(".down");
+
+    if(newDirection == "up")
+    {
+        upIcon.style.opacity = "100%";
+        downIcon.style.opacity = "0%"
+    }
+    else
+    {
+        upIcon.style.opacity = "0%";
+        downIcon.style.opacity = "100%"
+    }
+});
