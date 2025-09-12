5508d41deefb9ce69204128ae26b15507f10ee71
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-09-12 15:26:16 -0500

    Update styling for edit icons for tasks.
    Icons no longer hard to distinguish between background
    and each icon.

--- src/display.js
+++ src/display.js
@@ -494,10 +494,10 @@ export const display = (function () {
         if(parent.children.length == 0)
         {
             const pencilIcon = displaySVG({
-            className: "icon-pencil", // optional
-            viewBox: "0 0 24 24",
-            pathD: "M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z",
-            titleText: "pencil"
+                className: "icon-square-edit-outline", // optional
+                viewBox: "0 0 24 24",
+                pathD: "M5,3C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19H5V5H12V3H5M17.78,4C17.61,4 17.43,4.07 17.3,4.2L16.08,5.41L18.58,7.91L19.8,6.7C20.06,6.44 20.06,6 19.8,5.75L18.25,4.2C18.12,4.07 17.95,4 17.78,4M15.37,6.12L8,13.5V16H10.5L17.87,8.62L15.37,6.12Z",
+                titleText: "square-edit-outline"
             });
             parent.appendChild(pencilIcon);
 
